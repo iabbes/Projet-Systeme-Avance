d0d60eae77e21694609338e9fb12dbbd0b0f5cd5
Ökkeş Amoca <okkes2001@hotmail.fr>
2023-02-07 15:29:50 +0100

    merge commandes > main
    ça marche

--- headers/process.h
+++ headers/process.h
@@ -2,13 +2,19 @@
 #define PROCESS_H
 #include "utils.h"
 #include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 int findPipe(char* cmd, char** cmdPiped);
 void findSpace(char* cmd, char** cmdParsed);
+int findEpsilone(char* cmd, char** cmdPiped);
+int findRedirection(char* cmd, char** cmdPiped);
 int processCommand(char* cmd, char** cmdParsed, char** cmdPiped);
 int cmdManager(char** cmdParsed);
 void processArguments(char** parsed);
 void processArgumentsPipe(char** parsed, char** parsedpipe);
+void processArgumentsBackground(char **parsed);
+void processArgumentsRedirection(char **parsedCmd, char **parsedFile);
 char *strsep(char **stringp, const char *delim);
 
 #endif
--- sources/process.c
+++ sources/process.c
@@ -22,6 +22,48 @@ int findPipe(char *cmd, char **cmdPiped)
 }
 
 /**
+ * @brief find epsilone in command
+ * @param cmd command
+ * @param cmdParsed command parsed
+ * @return 0 if no epsilone, 1 otherwise
+ */
+int findEpsilone(char *cmd, char **cmdParsed)
+{
+    int i = 0;
+    while (i <= 3)
+    {
+        cmdParsed[i] = strsep(&cmd, "&");
+        if (cmdParsed[i] == NULL)
+        {
+            break;
+        }
+        i++;
+    }
+    return (cmdParsed[1] == NULL) ? 0 : 1;
+}
+
+/**
+ * @brief find redirection in command
+ * @param cmd command
+ * @param cmdParsed command parsed
+ * @return 0 if no redirection, 1 otherwise
+ */
+int findRedirection(char *cmd, char **cmdParsed)
+{
+    int i = 0;
+    while (i <= 3)
+    {
+        cmdParsed[i] = strsep(&cmd, ">");
+        if (cmdParsed[i] == NULL)
+        {
+            break;
+        }
+        i++;
+    }
+    return (cmdParsed[1] == NULL) ? 0 : 1;
+}
+
+/**
  * @brief find space in command
  * @param cmd command
  * @param cmdParsed command parsed
@@ -55,9 +97,15 @@ void findSpace(char *cmd, char **cmdParsed)
 int processCommand(char *cmd, char **cmdParsed, char **cmdPiped)
 {
     int piped = 0;
+    int epsilone = 0;
+    int redirection = 0;
     char *pipedCmd[2];
+
     piped = findPipe(cmd, pipedCmd);
-    if (!piped)
+    epsilone = findEpsilone(cmd, pipedCmd);
+    redirection = findRedirection(cmd, pipedCmd);
+
+    if ((!piped && !redirection) || epsilone)
     {
         findSpace(cmd, cmdParsed);
     }
@@ -67,7 +115,26 @@ int processCommand(char *cmd, char **cmdParsed, char **cmdPiped)
         findSpace(pipedCmd[1], cmdPiped);
     }
 
-    return (cmdManager(cmdParsed)) ? 0 : piped + 1;
+    if (cmdManager(cmdParsed))
+    {
+        return 0;
+    }
+    else if (epsilone)
+    {
+        return 2;
+    }
+    else if (piped)
+    {
+        return 3;
+    }
+    else if (redirection)
+    {
+        return 4;
+    }
+    else
+    {
+        return 1;
+    }
 }
 
 /**
@@ -201,6 +268,90 @@ void processArgumentsPipe(char **parsed, char **parsedpipe)
     // Wait for the child processes to finish
     wait(&statutExec1);
     wait(&statutExec2);
+}
+
+/**
+ * @brief function which handle our commands
+ * @param parsed command parsed
+ * @param parsedpipe command parsed pipe
+ * @return 0 if we don't have a command, 1 otherwise
+ */
+void processArgumentsRedirection(char** parsedCmd, char **parsedFile)
+{
+    int fd;
+    int stdout_copy;
+
+    // Sauvegarde la sortie standard
+    stdout_copy = dup(STDOUT_FILENO);
+    if (stdout_copy == -1)
+    {
+        perror("dup");
+        exit(EXIT_FAILURE);
+    }
+
+    // Ouvre le fichier de sortie en écriture, remplace son contenu s'il existe déjà
+    fd = open(parsedFile[0], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+    if (fd == -1)
+    {
+        perror("open");
+    }
+
+    // Redirige la sortie standard vers le fichier de sortie
+    if (dup2(fd, STDOUT_FILENO) == -1)
+    {
+      perror("dup2");
+    }
+
+    // Exécute la commande shell
+    if (system(parsedCmd[0]) == -1)
+    {
+      perror("system");
+    }
+
+    // Ferme le descripteur de fichier
+    if (close(fd) == -1)
+    {
+      perror("close");
+    }
+
+    // Restaure la sortie standard
+    if (dup2(stdout_copy, STDOUT_FILENO) == -1)
+    {
+      perror("dup2");
+      exit(EXIT_FAILURE);
+    }
+
+    // Ferme la copie de la sortie standard
+    if (close(stdout_copy) == -1)
+    {
+      perror("close");
+      exit(EXIT_FAILURE);
+    }
+}
+
+/**
+ * @brief function which handle our commands
+ * @param parsed command parsed
+ * @return 0 if we don't have a command, 1 otherwise
+ */
+void processArgumentsBackground(char **parsed)
+{
+    pid_t pid = fork();
+
+    if (pid == 0)
+    {
+        // Exécution de la commande en arrière-plan
+        execl("/bin/sh", "sh", "-c", parsed[0], (char *)NULL);
+    }
+    else if (pid < 0)
+    {
+        perror("Erreur de fork");
+    }
+    else
+    {
+        // Attente de la fin de l'exécution de la commande
+        printf("Le PID du processus en arrière-plan est %d\n", pid);
+    }
 }
 
 /**
